652d1b17b980a20895e47c4ddcd04a537d62a081
AndrewNikitin127 <donandrain153@gmail.com>
2023-10-31 19:03:34 +0500

    cleaned up

--- src/StringSchema.js
+++ src/StringSchema.js
@@ -1,10 +1,6 @@
 export default class StringSchema {
   validators = [(val) => typeof val === 'string'];
 
-  constructor(newValidators) {
-    if (newValidators) this.validators = newValidators;
-  }
-
   isValid(val) {
     return this.validators.every((validator) => validator(val));
   }
@@ -12,7 +8,7 @@ export default class StringSchema {
   length(minLength, maxLength) {
     const validator = (val) => {
       if (!maxLength) return minLength <= val.length;
-      return (minLength <= val.length) && (val.length <= maxLength);
+      return minLength <= val.length <= maxLength;
     };
     this.validators[1] = validator;
     return this;
